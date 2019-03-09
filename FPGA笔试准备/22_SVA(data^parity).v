3fb0543a029587b607c6127b4fb024e654974929
lidongyang1986 <lidongyang@ele.uri.edu>
2019-03-08 19:36:03 -0500

    Create 22_SVA(data^parity).v

---
+++ FPGA笔试准备/22_SVA(data^parity).v
@@ -0,0 +1,42 @@
+/*
+module check_par(clk, parity, data);
+input clk, parity;
+input [31:0] data;
+
+property p_check_par;
+  @(posedge clk)  (^(data^parity)) == 1'b0;
+endproperty
+a_check_par: assert property(p_check_par);
+endmodule
+  */
+
+/*
+module dffChecker (clk, rst_, d, q);
+parameter WIDTH = 8;
+input clk, rst_;
+input [WIDTH-1:0] d, q;
+property d_q_property_0;
+@(posedge clk) !rst_ |->##1 (q == 8'h0);
+endproperty
+endmodule
+*/
+
+
+module dffChecker (clk, rst_, d, q);
+parameter WIDTH = 8;
+input clk, rst_;
+  
+input [WIDTH-1:0] d, q;
+  
+logic [3:0] delay,a,b;
+
+property assert_check;
+  @(posedge clk) disable iff(rst_)
+  $rose(a) |-> ##(delay) b == 1;
+endproperty : assert_check
+  
+  
+  
+endmodule
+
+
