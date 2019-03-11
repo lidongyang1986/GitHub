e88906b7e3a1c05b202af2a09afbfc30c6f930b1
lidongyang1986 <lidongyang@ele.uri.edu>
2019-03-10 20:39:09 -0400

    Create 15 dual port 双向端口.v

---
+++ FPGA笔试准备/15 dual port 双向端口.v
@@ -0,0 +1,22 @@
+//http://www.asic.co.in/Index_files/verilog_interview_questions2.htm
+
+
+module bidirec (oe, clk, inp, outp, bidir);
+
+// Port Declaration
+input oe;
+input clk;
+input [7:0] inp;
+output [7:0] outp;
+inout [7:0] bidir; 
+reg [7:0] a;
+reg [7:0] b;
+assign bidir = oe ? a : 8'bZ ;
+assign outp = b;
+// Always Construct
+always @ (posedge clk)
+begin
+b <= bidir;
+a <= inp;
+end
+endmodule
