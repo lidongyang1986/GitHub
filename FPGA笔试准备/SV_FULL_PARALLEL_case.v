605f8cb4452c7a6d2842ac672018d719ccb73722
lidongyang1986 <lidongyang@ele.uri.edu>
2019-03-10 20:16:04 -0400

    Update SV_FULL_PARALLEL_case.v

--- FPGA笔试准备/SV_FULL_PARALLEL_case.v
+++ FPGA笔试准备/SV_FULL_PARALLEL_case.v
@@ -23,6 +23,24 @@ endmodule
 /////////////////////////////////
 https://asic-interview.blogspot.com/2010/04/systemverilog-interview-question-5.html
 
+
+e.g. Full case, sel=2'b11 will be covered by default statement.
+     The x-assignment will also be treated as a don'tcare for synthesis, which may allow the synthesis tool to further optimize the synthesized design. It's the potentially causing a mismatch to occur between simulation and synthesis. To insure that the pre-synthesis and post-synthesis simulations match, the case default could assign the y-output to either a
+predetermined constant value, or to one of the other multiplexer input values
+
+module mux3c
+(output reg y,
+input [1:0] sel,
+input a, b, c);
+always @*
+case (sel)
+2'b00: y = a;
+2'b01: y = b;
+2'b10: y = c;
+default: y = 1'bx;
+endcase
+endmodule
+
 FULL
 SystemVerilog use priority modified case statement to solve the full case problem:
 e.g.
@@ -31,6 +49,14 @@ priority case (...)
 endcase
 
 
+  
+  
+  
+  
+  
+  
+  
+  
 
 ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 PARALLEL
